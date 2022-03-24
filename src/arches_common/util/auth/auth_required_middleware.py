a63159580d6349ec56ce68d08092859f4234e65b
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2022-03-24 08:21:46 -0700

    82 - Added auth required redirect middleware to common assets

--- src/arches_common/util/auth/auth_required_middleware.py
+++ src/arches_common/util/auth/auth_required_middleware.py
@@ -3,9 +3,9 @@ from django.contrib.auth.middleware import AuthenticationMiddleware
 
 
 class AuthRequiredMiddleware(AuthenticationMiddleware):
+    # Big-hammer approach - redirect anyone not authenticated to external URL
+
     def process_request(self, request):
         if not request.user.is_authenticated:
-            return HttpResponseRedirect(
-                "https://www2.gov.bc.ca/gov/content/industry/natural-resource-use/fossil-management/"
-            )
+            return HttpResponseRedirect("https://www2.gov.bc.ca/gov/content/industry/natural-resource-use/fossil-management/")
         return None
