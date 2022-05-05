61d0b334cfe9a04d1f636ef9b493613d143af18a
Brett Ferguson <brett@qedsystems.ca>
2022-05-04 17:14:47 -0700

    Sprint4 PR (#23)
    * 76 - Added custom map popup for bedrock geology overlay
    
    * 76 - Add ability to show > 4 lines of popup descriptor
    
    * 76 - Fixed popup title width, added original description field to bedrock overlay popup
    
    * 103 - Copy existing map-filter.js from core for updates
    
    * 103 - Added copy of core map_filter.py for project updates
    
    * 103 - Updates to allow multiple filter features
    
    * 76 - Update BCRHP to use custom map popup
    
    * 33 - Model updates for fossil types and note fields
    
    * 76 - Added fill to bedrock geology overlay
    
    * 103 - First stab at solving URL length issue by changing search GET to POST
    
    * 103 - Add ability to delete filter geometries and control points
    
    * 103 - Added arches core views/search.js for update, updated filter icon to funnel in map popup
    
    * 103 - Submit search ajax call as POST if the URL is larger than 2048 characters
    
    * 103 - Hide popup edit button for non-arches features
    
    * Remove important fossil area overlay
    
    * Added core map-editor viewmodel and map template for override
    
    * Reverted reformat of core files
    
    * Reworked select cadastral feature as geometry functionality for new core popup functionality
    
    * Fossils - Only show edit button if Arches Resource
    
    * 103 - Submit long map resource filters by POST
    
    * 103 - Implement geometry intersect/union configuration
    
    * 103 - Delete feature being drawn when changing filter draw mode
    
    * 114 - Added package changes for reworked cadastral select
    
    * 103 - Update the default map filter config with select by feature using pkg prelim sql

---
+++ src/arches_fossils/arches_fossils/media/js/utils/map-popup-provider.js
@@ -0,0 +1,98 @@
+define([
+    'knockout',
+    'underscore',
+    'text!templates/views/components/map_popup/toggle-map-popup.htm'
+], function (ko, _, toggle_template) {
+    var popupDataProvider = {
+            layerConfigs: {
+                "WHSE_MINERAL_TENURE.GEOL_BEDROCK_UNIT_POLY_SVW":
+                    { displayname: [{"key": "PROJECT_NAME", title:""}],
+                        map_popup: [{"key": "MAXIMUM_AGE_NAME", "title": "Max. Age:"},
+                            {"key": "MINIMUM_AGE_NAME", "title": "Min Age:"},
+                            {"key": "AGE_GROUP", "title": "Age Group:"},
+                            {"key": "GROUP_SUITE_NAME", "title": "Group Suite:"},
+                            {"key": "ORIGINAL_DESCRIPTION", "title": "Description:"},
+                        ]},
+                "WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW":
+                    {displayname: [{"key": "PID", title:""}],
+                        "map_popup": [{key: "OWNER_TYPE", title: "Owner Type: "},
+                            {key: "PLAN_NUMBER", title: "Plan #: "},
+                            {key: "PARCEL_CLASS", title: "Parcel Class: "},
+                            {key: "MUNICIPALITY", title: "Municipality: "},
+                            {key: "REGIONAL_DISTRICT", title: "Parcel Status: "},
+                            {key: "PARCEL_STATUS", title: "Parcel Status: "},
+                            {key: "PARCEL_START_DATE", title: "Parcel Start Date: "},
+                            {key: "FEATURE_AREA_SQM", title: "Size (SQM): "},
+                        ]}
+
+            },
+            filterLayers: [
+                "WHSE_MINERAL_TENURE.GEOL_BEDROCK_UNIT_POLY_SVW",
+                "c66518e2-10c6-11ec-adef-5254008afee6" /* Important Areas Resource layer */
+            ],
+
+            isFeatureClickable: function(feature, drawMode){
+                // console.log("fossils_popup_provider.isFeatureClickable()")
+                if (typeof drawMode !== 'undefined' && drawMode !== null && drawMode !== 'filter_by_feature')
+                    return false;
+                if (feature.sourceLayer in popupDataProvider.layerConfigs)
+                    return true;
+                return feature.properties.resourceinstanceid;
+            },
+
+            getPopupTemplate: function(features){
+                return toggle_template;
+            },
+            isSelectableAsFilter: function(feature) {
+                return popupDataProvider.filterLayers.indexOf(feature.sourceLayer) !== -1;
+            },
+            processData: function(dataFeatures) {
+                console.log(dataFeatures);
+                dataFeatures.popupFeatures.forEach(featureData => {
+                    if (!featureData.displayname && featureData.feature.sourceLayer in this.layerConfigs)
+                    {
+                        featureData.displayname = this.getDisplayValue(featureData, this.layerConfigs[featureData.feature.sourceLayer].displayname);
+                    }
+                    if (!featureData.map_popup && featureData.feature.sourceLayer in this.layerConfigs)
+                    {
+                        featureData.map_popup = this.getDisplayValue(featureData, this.layerConfigs[featureData.feature.sourceLayer].map_popup);
+                    }
+                    featureData.showAll = ko.observable(false);
+                    featureData.toggleShowAll = function()
+                    {
+                        featureData.showAll(!featureData.showAll());
+                    };
+                    featureData.showExpandButton = function(container, event)
+                    {
+                        console.log(container);
+                        var div = $(container).siblings(".hover-feature");
+                        if (div.length > 0)
+                        {
+                            return (div[0].offsetWidth < div[0].scrollWidth ||
+                                div[0].offsetHeight < div[0].scrollHeight);
+                        }
+                    }
+                });
+                return dataFeatures;
+            },
+            getDisplayValue: function(featureData, properties) {
+                return ko.computed(function()
+                {
+                    let returnValue = "";
+                    properties.forEach(property => {
+                        if (featureData[property.key])
+                        {
+                            if (returnValue != "") returnValue += "<br>"
+                            if (property.title)
+                            {
+                                returnValue +="<b>"+property.title+"</b> "
+                            }
+                            returnValue += featureData[property.key];
+                        }
+                    });
+                    return returnValue;
+                });
+            }
+        };
+    return popupDataProvider;
+});
---
+++ src/arches_fossils/arches_fossils/pkg/preliminary_sql/update_map_filter_config.sql
@@ -0,0 +1,4 @@
+update search_component set componentpath = 'views/components/search/map-filter' ,
+                            modulename = 'map_filter_fossils.py',
+                            classname = 'MapFilterFossils'
+where searchcomponentid = '09d97fc6-8c83-4319-9cef-3aaa08c3fbec';
