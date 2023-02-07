b5d506ef392b341c95c9c8097d1b77161da76172
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2023-02-07 07:41:12 -0800

    233 - Bypass auth required check for localhost node-fetch requests

--- src/arches_common/util/auth/auth_required_middleware.py
+++ src/arches_common/util/auth/auth_required_middleware.py
@@ -1,11 +1,23 @@
 from django.http.response import HttpResponseRedirect
 from django.contrib.auth.middleware import AuthenticationMiddleware
+from arches.app.models.system_settings import settings
 
 
 class AuthRequiredMiddleware(AuthenticationMiddleware):
     # Big-hammer approach - redirect anyone not authenticated to external URL
 
+    def bypass_auth(self, request):
+        # print(str(request.META.get('REMOTE_ADDR')))
+        # print(str(request.META.get('HTTP_X_FORWARDED_FOR')))
+        request_source = request.META.get('REMOTE_ADDR') if request.META.get(
+            'HTTP_X_FORWARDED_FOR') is None else request.META.get('HTTP_X_FORWARDED_FOR')
+        return request_source in settings.AUTH_BYPASS_HOSTS and request.META.get('HTTP_USER_AGENT').startswith(
+            "node-fetch/1.0")
+
     def process_request(self, request):
         if not request.user.is_authenticated:
-            return HttpResponseRedirect("https://www2.gov.bc.ca/gov/content/industry/natural-resource-use/fossil-management/")
+            if not self.bypass_auth(request):
+                return HttpResponseRedirect(settings.AUTH_NOACCESS_URL)
+            else:
+                print("Bypassing auth")
         return None
