30c07c470ad7b48a5ad3e991e30013701fd737e1
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2023-02-10 07:21:43 -0800

    185 - Added conditional formatting and labels to FRPC layer 214 - Don't display FRPC layer by default

--- src/arches_fossils/arches_fossils/pkg/preliminary_sql/create_resource_overlay_package.sql
+++ src/arches_fossils/arches_fossils/pkg/preliminary_sql/create_resource_overlay_package.sql
@@ -26,6 +26,14 @@ begin
              (select * from tiles where nodegroupid = 'd08d1b72-70d7-11ed-9c1b-5254004d77d3'::uuid) rankt
         where p_resourceinstanceid = namet.resourceinstanceid::uuid
         and p_resourceinstanceid = namet.resourceinstanceid::uuid;
+    elsif nodeid = '4fe7beb2-1f3a-11ed-a99d-5254008afee6'::uuid then -- FRPC Areas
+        select jsonb_build_object( 'name', namet.tiledata->'5f809ec0-1f3a-11ed-ab70-5254008afee6'->'en'->>'value',
+                                   'ranking', rankt.tiledata -> '5d097acc-1f3a-11ed-ab70-5254008afee6')
+        into data
+        from (select * from tiles where nodegroupid = '5f809ec0-1f3a-11ed-ab70-5254008afee6'::uuid) namet,
+             (select * from tiles where nodegroupid = '5d097acc-1f3a-11ed-ab70-5254008afee6'::uuid) rankt
+        where p_resourceinstanceid = namet.resourceinstanceid::uuid
+          and p_resourceinstanceid = rankt.resourceinstanceid::uuid;
     else
             data = '{}'::jsonb;
     end if;
