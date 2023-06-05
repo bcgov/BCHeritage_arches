98b28668101fb191a145362aefc1e71356c7afbd
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2023-06-05 10:16:53 -0700

    193 - Create proxy for tiles servers 393 - Allow longer search results card

--- src/arches_bchp/arches_bchp/urls.py
+++ src/arches_bchp/arches_bchp/urls.py
@@ -3,7 +3,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.urls.resolvers import RegexPattern
+# from arches_bchp.views.api import MVT
+from .views.map import BCTileserverProxyView
 
+# uuid_regex = settings.UUID_REGEX
 
 print("settings.MEDIA_URL: %s"%settings.MEDIA_URL)
 print("settings.MEDIA_ROOT: %s"%settings.MEDIA_ROOT)
@@ -27,7 +30,13 @@ else:
 
 
 urlpatterns = [
-    bc_url_resolver,
+                  url(r"^%sbctileserver/(?P<path>.*)$" % settings.BCGOV_PROXY_PREFIX, BCTileserverProxyView.as_view()),
+                  # url(
+                  #     r"^AAAAA%smvt/(?P<nodeid>%s)/(?P<zoom>[0-9]+|\{z\})/(?P<x>[0-9]+|\{x\})/(?P<y>[0-9]+|\{y\}).pbf$" % (settings.BCGOV_PROXY_PREFIX, uuid_regex),
+                  #     MVT.as_view(),
+                  #     name="mvt",
+                  #     ),
+                  bc_url_resolver,
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.SHOW_LANGUAGE_SWITCH is True:
---
+++ src/arches_common/views/map.py
@@ -0,0 +1,26 @@
+import urllib3
+from arches.app.views.map import TileserverProxyView
+from django.conf import settings
+
+class BCTileserverProxyView(TileserverProxyView):
+    upstream_urls = settings.BC_TILESERVER_URLS
+    """
+    Subclass of the TileserverProxyView that has multiple upstream servers.
+        - the BC_TILESERVER_URLS is a dict with source->upstream URL mapping
+        - the request URL must have the source parameter set. If not set it defaults to the parent upstream value
+    """
+    def __init__(self, *args, **kwargs):
+        super(BCTileserverProxyView, self).__init__(*args, **kwargs)
+        # Setup outbound proxy if it is configured
+        if hasattr(settings, 'TILESERVER_OUTBOUND_PROXY')  and settings.TILESERVER_OUTBOUND_PROXY:
+            print("Setting outbound proxy to %s" % settings.TILESERVER_OUTBOUND_PROXY)
+            self.http = urllib3.ProxyManager(settings.TILESERVER_OUTBOUND_PROXY)
+
+
+    def get_request_headers(self):
+        proxy_source = self.request.GET.get('source', '')
+        # print("\tProxy source: %s" % proxy_source)
+        if proxy_source in self.upstream_urls:
+            self.upstream = self.upstream_urls[proxy_source]
+
+        return super(BCTileserverProxyView, self).get_request_headers()
