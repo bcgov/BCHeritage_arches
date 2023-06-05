5224d6b8b497d5baf870b16fe220f2be3c633cd9
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2023-06-05 16:53:04 -0700

    193 - Update config for fossils overlays

--- src/arches_fossils/arches_fossils/urls.py
+++ src/arches_fossils/arches_fossils/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.urls.resolvers import RegexPattern
 from arches_fossils.views.api import MVT
+from arches_fossils.views.map import BCTileserverProxyView
 
 uuid_regex = settings.UUID_REGEX
 
@@ -19,6 +20,7 @@ if settings.BCGOV_PROXY is True:
         #print(pattern.pattern)
 
 urlpatterns = [
+                  url(r"^%sbctileserver/(?P<path>.*)$" % settings.BCGOV_PROXY_PREFIX, BCTileserverProxyView.as_view()),
                   url(
                       r"^%smvt/(?P<nodeid>%s)/(?P<zoom>[0-9]+|\{z\})/(?P<x>[0-9]+|\{x\})/(?P<y>[0-9]+|\{y\}).pbf$" % (settings.BCGOV_PROXY_PREFIX, uuid_regex),
                       MVT.as_view(),
