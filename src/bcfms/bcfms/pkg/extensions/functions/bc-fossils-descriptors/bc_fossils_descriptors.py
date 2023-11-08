183aa648c403d7cdea2ca33da444d691200e1e29
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2023-11-07 22:51:15 -0800

    542 - Updated collection event location fields

--- src/bcfms/bcfms/pkg/extensions/functions/bc-fossils-descriptors/bc_fossils_descriptors.py
+++ src/bcfms/bcfms/pkg/extensions/functions/bc-fossils-descriptors/bc_fossils_descriptors.py
@@ -168,7 +168,7 @@ class BCFossilsDescriptors(AbstractPrimaryDescriptorsFunction):
 
         start_date_node = models.Node.objects.filter(graph=resource.graph_id) .filter(name="Collection Start Year") .first()
         end_date_node = models.Node.objects.filter(graph=resource.graph_id) .filter(name="Collection End Year") .first()
-        geographical_node = models.Node.objects.filter(graph=resource.graph_id) .filter(name="General Location") .first()
+        geographical_node = models.Node.objects.filter(graph=resource.graph_id) .filter(name="Location Descriptor") .first()
 
         if start_date_node:
             value = self._get_value_from_node(
