8555634867bb5354dffdb6d6cf8a668ba8f98059
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2024-01-14 07:32:15 -0800

    752 - Interim commit - partial rework of bchp-site report

--- src/bcrhp/bcrhp/media/js/viewmodels/bcrhp-site.js
+++ src/bcrhp/bcrhp/media/js/viewmodels/bcrhp-site.js
@@ -11,14 +11,121 @@ define([
         var self = this;
 
         MapReportViewModel.apply(this, [params]);
-        this.isSummaryCard = function(cardName) {
-            if (cardName === "Borden Number")
-            {
-                return true;
-            }
-            return false;
+        var getAllWidgets = function(card) {
+            return _.flatten([ko.unwrap(card.tiles).length === 0 ? [] : ko.unwrap(card.widgets),
+                _.map(card.cards(), subcard => {return getAllWidgets(subcard); })]);
+        }
+        var getAllTiles = function(card) {
+            return _.flatten([ko.unwrap(card.tiles),
+                _.map(card.cards(), subcard => {return getAllTiles(subcard); })]);
+        }
+
+        var widgets = _.flatten(_.map(params.report.cards, card => {return getAllWidgets(card)}));
+
+        var tiles = _.flatten(_.map(params.report.cards, card => {return getAllTiles(card)}));
+
+        var getWidgetForAlias = function(node_alias){
+            var widget = _.find(widgets, widget => {
+                return ko.unwrap(widget.node.alias) === node_alias;
+            })
+            return widget;
+        };
+        // Used by template to get widgets for config
+        this.getWidgetForAlias = function(node_alias) {
+            return getWidgetForAlias(node_alias);
+        }
+
+        var getTilesForAlias = function(node_alias)
+        {
+            let widget = getWidgetForAlias(node_alias);
+
+            let tiles = !widget ? [] : _.filter(ko.unwrap(self.tiles), tile=>{
+                return widget.node.nodeid in tile.data;
+            });
+            tiles = _.map(tiles, tile => {
+                return ko.observable(tile);
+            })
+            return tiles;
+        }
+
+        this.getTilesForAlias = function(node_alias)
+        {
+            return getTilesForAlias(node_alias);
         };
 
+        var getValueFromTile = function(tile, widget)
+        {
+            return (!tile || !ko.unwrap(tile) || !widget) ? null : ko.unwrap(ko.unwrap(tile).data[widget.node.nodeid]);
+        }
+
+        var getNodeValues = function(node_alias, make_observable = false) {
+            var widget = getWidgetForAlias(node_alias);
+            if (widget === null)
+            {
+                console.log(`Node with alias ${node_alias} does not exist.`)
+                return [];
+            }
+
+            var values = [];
+            _.each(ko.unwrap(tiles), tile => {
+                var value = getValueFromTile(tile, widget)
+                if (!!value)
+                    values.push(make_observable ? ko.observable(value) : value);
+            });
+            return values;
+        }
+
+        this.getNodeValues = function(node_alias)
+        {
+            return getNodeValues(node_alias, true);
+        }
+
+        this.nodesHaveData = function(aliases, requireAll = false)
+        {
+            let values =  [];
+            _.each(aliases, alias => {
+                values.push(getNodeValues(alias));
+            });
+            return !!_.find(_.flatten(values), value => {
+                return ko.unwrap(value) != null
+            });
+        }
+
+        this.getFirstNodeValue = function(alias) {
+            return ko.observable(getNodeValues(alias)[0]);
+        };
+
+        this.getResourceInstanceValues = function(aliases)
+        {
+            if (!Array.isArray(aliases))
+            {
+                aliases = [aliases]
+            }
+            let widgets = _.map(aliases, alias=> {
+                return {"alias": alias, "widget": getWidgetForAlias(alias)}
+            });
+
+            let resourceValues = [];
+            _.each(ko.unwrap(tiles), tile => {
+                _.each(widgets, widget => {
+                    let value = getValueFromTile(tile, widget.widget);
+                    if (!!value)
+                    {
+                        resourceValues.push({[widget.alias]: ko.observable(value)});
+                    }
+                })
+            });
+            return resourceValues;
+        }
+
+        this.clickUrl = function(data, event) {
+            let url = event.currentTarget.getElementsByTagName('a')[0]['href'];
+            let filename = event.currentTarget.getElementsByTagName('a')[0].text.trim();
+            window.open(url, filename);
+        }
+
+
+        /* Old config ... to remove */
         this.getCardWithName = function(cardName) {
             if (_.isArray(cardName))
             {
@@ -54,6 +161,7 @@ define([
             }
             return null;
         };
+        /* Old config ... to remove */
 
     };
 });
