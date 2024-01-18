76d7ffc7e8f279bb38f62823016998d1fa577bdb
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2024-01-18 12:30:32 -0800

    708 - Interim commit of Data BC export view

---
+++ src/bcrhp/bcrhp/pkg/business_data/resource_views/0002_create_onerow_view.sql
@@ -0,0 +1,250 @@
+-- Materialized views to support the Data BC export
+drop materialized view mv_borden_number;
+create materialized view mv_borden_number as
+select resourceinstanceid, borden_number->'en'->>'value' borden_number from heritage_site.borden_number;
+create index bn_idx on mv_borden_number (resourceinstanceid);
+
+drop materialized view mv_site_names;
+create materialized view mv_site_names as
+select resourceinstanceid, __arches_get_concept_label(name_type) name_type,
+       name->'en'->>'value' name
+from heritage_site.site_names;
+create index sn_idx on mv_site_names (resourceinstanceid);
+
+
+select * from mv_property_address;
+drop materialized view mv_property_address cascade;
+create materialized view mv_property_address as
+select resourceinstanceid,
+       tileid property_address_id,
+       street_address->'en'->>'value' street_address,
+       city->'en'->>'value' city,
+       __arches_get_concept_label(province) province,
+       postal_code->'en'->>'value' postal_code,
+       locality->'en'->>'value' locality,
+       location_description->'en'->>'value' location_description
+from heritage_site.bc_property_address pa;
+create index pa_ri_idx1 on mv_property_address(resourceinstanceid);
+create index pa_ri_idx2 on mv_property_address(resourceinstanceid, property_address_id);
+
+select * from mv_legal_description;
+drop materialized view mv_legal_description;
+create materialized view mv_legal_description as
+select resourceinstanceid,
+       bc_property_address,
+       pid,
+       pin,
+       legal_description
+from heritage_site.bc_property_legal_description ld;
+create index ld_ri_idx on mv_legal_description(resourceinstanceid, bc_property_address);
+
+
+create materialized view mv_bc_right as
+with unnested as (
+select resourceinstanceid,
+       tileid bc_right_id,
+       officially_recognized_site,
+       __arches_get_concept_label(registration_status) registration_status,
+       unnest(registry_types)  registry_type
+        from heritage_site.bc_right)
+select resourceinstanceid,
+       bc_right_id,
+       officially_recognized_site,
+       registration_status,
+       array_agg(__arches_get_concept_label(registry_type)) registry_types from unnested
+group by resourceinstanceid,
+         bc_right_id,
+         officially_recognized_site,
+        registration_status;
+create index br_ri_idx on mv_bc_right(resourceinstanceid, bc_right_id);
+
+create materialized view mv_government as
+select resourceinstanceid, government_name->'en'->>'value' government_name,
+       __arches_get_concept_label(government_type) government_type
+from government.government_name;
+-- left join government.government_name gn on (pe.responsible_government[0]->>'resourceId')::uuid = gn.resourceinstanceid;
+
+select pe.* from mv_protection_event pe join (
+select resourceinstanceid, count(*) from mv_protection_event
+where authority = 'Provincial'
+group by resourceinstanceid having count(*) > 1) dups on dups.resourceinstanceid = pe.resourceinstanceid
+order by pe.resourceinstanceid;
+
+drop materialized view mv_protection_event;
+create materialized view mv_protection_event as
+select pe.resourceinstanceid,
+       bc_right bc_right_id,
+       designation_or_protection_start_date,
+       designation_or_protection_end_date,
+       reference_number->'en'->>'value' reference_number,
+       protection_notes->'en'->>'value' protection_notes,
+       __arches_get_concept_label(la.authority) authority,
+       __arches_get_concept_label(la.legal_instrument) legal_instrument,
+       la.act_section->'en'->>'value' act_section,
+       __arches_get_concept_label(la.recognition_type) recognition_type,
+       gn.government_name,
+       gn.government_type
+--        gn.government_type->'en'->>'value' government_type
+from heritage_site.protection_event pe
+    join legislative_act.authority la on (pe.legislative_act[0]->>'resourceId')::uuid = la.resourceinstanceid
+    left join mv_government gn on (pe.responsible_government[0]->>'resourceId')::uuid = gn.resourceinstanceid;
+;
+create index mv_pe_idx on mv_protection_event(resourceinstanceid, bc_right_id);
+
+select * from mv_chronology;
+drop materialized view mv_chronology;
+create materialized view mv_chronology as
+select resourceinstanceid,
+       start_year,
+       end_year,
+       dates_approximate,
+       __arches_get_concept_label(chronology) event,
+       information_source->'en'->>'value' source,
+       chronology_notes->'en'->>'value' event_notes
+from heritage_site.chronology;
+create index mv_chron_idx on mv_protection_event(resourceinstanceid);
+
+select * from mv_site_record_admin;
+drop materialized view mv_site_record_admin cascade;
+create materialized view mv_site_record_admin as
+select resourceinstanceid,
+       restricted,
+       __arches_get_concept_label(bcrhp_submission_status) bcrhp_submission_status,
+       __arches_get_concept_label(crhp_submission_status) crhp_submission_status,
+       date_submitted_to_crhp,
+       federal_id_number
+from heritage_site.site_record_admin;
+create index mv_sra_idx on mv_site_record_admin(resourceinstanceid);
+
+select * from mv_heritage_function;
+drop materialized view mv_heritage_function cascade;
+create materialized view mv_heritage_function as
+    select resourceinstanceid,
+           __arches_get_concept_label(unnest(functional_state)) state_period,
+           array_agg(__arches_get_concept_label(functional_category))  functional_states
+    from heritage_site.heritage_function group by resourceinstanceid,
+                                                  __arches_get_concept_label(unnest(functional_state));
+create index mv_hf_idx on mv_heritage_function(resourceinstanceid);
+
+select * from mv_construction_actors;
+drop materialized view mv_construction_actors cascade;
+create materialized view mv_construction_actors as
+select resourceinstanceid,
+       __arches_get_concept_label(construction_actor_type) actor_type,
+       array_agg(construction_actor->'en'->>'value') actors
+        from heritage_site.construction_actors
+group by resourceinstanceid, __arches_get_concept_label(construction_actor_type);
+create index mv_ca_idx on mv_construction_actors(resourceinstanceid);
+
+drop materialized view mv_site_boundary cascade;
+create materialized view mv_site_boundary as
+select b.resourceinstanceid,
+       site_boundary,
+       st_area(site_boundary::geography) area_sqm,
+       st_y(st_centroid(site_boundary)) site_centroid_latitude,
+       st_x(st_centroid(site_boundary)) site_centroid_longitude,
+       int4(ceil((st_x(st_centroid(site_boundary)) + 180.0)/6)) utmzone,
+       trunc(st_y(st_transform(st_centroid(site_boundary), 26910))) utmnorthing,
+       trunc(st_x(st_transform(st_centroid(site_boundary), 26910))) utmeasting
+       from heritage_site.site_boundary b join heritage_site.borden_number bn on b.resourceinstanceid = bn.resourceinstanceid;
+create index mv_sb_idx on mv_site_boundary(resourceinstanceid);
+
+
+-- select * from MV_HISTORIC_ENVIRO_ONEROW_SITE order by borden_number;
+-- select * from MV_HISTORIC_ENVIRO_ONEROW_SITE where borden_number in ('DcRu-235', 'DcRu-234');
+-- select * from V_HISTORIC_ENVIRO_ONEROW_SITE where borden_number in ('DcRu-235', 'DcRu-234');
+
+-- drop materialized view if exists MV_HISTORIC_ENVIRO_ONEROW_SITE;
+-- refresh materialized view MV_HISTORIC_ENVIRO_ONEROW_SITE;
+
+drop view if exists V_HISTORIC_ENVIRO_ONEROW_SITE;
+-- create materialized view MV_HISTORIC_ENVIRO_ONEROW_SITE as
+create or replace view V_HISTORIC_ENVIRO_ONEROW_SITE as
+select distinct bn.resourceinstanceid,
+                bn.borden_number,
+                case when br.officially_recognized_site then 'Y' else 'N' end ishistoricsite,
+                br.registration_status registrationstatus,
+                cn.name                                 common_name,
+                array_to_string(other_name.names, '; ') other_name,
+                array_to_string(prop.pid, '; ')         parcelid,
+                prop.street_address,
+                prop.city,
+                prop.locality,
+                prop.province,
+                br.authorities->>'government_name' government_name,
+                br.authorities->>'authority' governmentlevel,
+                br.authorities->>'recognition_type' protectiontype,
+                to_char(to_date(br.authorities->>'start_date', 'YYYY-MM-DD'), 'YYYY')::int startdate,
+                case when mc.chronology is null then null else to_char(to_date(chronology[0]->>'start_year', 'YYYY-MM-DD'), 'YYYY') end date_fromdate,
+                case when mc.chronology is not null and (chronology[0]->>'dates_approximate')::boolean then 'Circa' end date_fromqualifier,
+                case when mc.chronology is null then null else chronology[0]->>'event' end date_histdatetype,
+--                 case when msra.crhp_submission_status = 'Approved' then 'Y' else 'N' end is_accepted_by_feds,
+                ca.actors->>'Architect / Designer' architect_name,
+                ca.actors->>'Builder' builder_name,
+                hf.functional_states->>'Current' current_function,
+                hf.functional_states->>'Historic' historic_function,
+                msb.area_sqm dimensions_area,
+                msb.site_centroid_latitude gislatitude,
+                msb.site_centroid_longitude gislongitude,
+                msb.utmzone,
+                msb.utmnorthing,
+                msb.utmeasting,
+                'https://apps.nrs.gov.bc.ca/int/bcrhp/report/'||bn.resourceinstanceid site_url
+--                 prop.location_description,
+--                 prop.pin,
+--                 prop.legal_description,
+--                 msra.restricted,
+--                 msra.bcrhp_submission_status,
+--                 msra.date_submitted_to_crhp,
+--                 msra.federal_id_number
+from mv_borden_number bn
+         left join (select resourceinstanceid, name from mv_site_names where name_type = 'Common') cn on cn.resourceinstanceid = bn.resourceinstanceid
+         left join (select resourceinstanceid, array_agg(name) names from mv_site_names where name_type = 'Other' group by resourceinstanceid) other_name on other_name.resourceinstanceid = bn.resourceinstanceid
+         left join mv_site_names sn on sn.resourceinstanceid = bn.resourceinstanceid
+         left join (select pa.resourceinstanceid, pa.property_address_id, pa.street_address, pa.city, pa.province,
+                           pa.postal_code, pa.locality, pa.location_description,
+                           array_remove(array_agg(distinct mld.pin->'en'->>'value'), '') pin,
+                           array_remove(array_agg(distinct mld.pid->'en'->>'value'), '') pid,
+                           array_remove(array_agg(distinct mld.legal_description->'en'->>'value'), null) legal_description
+                    from mv_property_address pa left join mv_legal_description mld
+                                                          on pa.resourceinstanceid = mld.resourceinstanceid and pa.property_address_id = mld.bc_property_address
+                    group by pa.resourceinstanceid, pa.property_address_id, pa.street_address, pa.city, pa.province,
+                             pa.postal_code, pa.locality, pa.location_description) prop on bn.resourceinstanceid = prop.resourceinstanceid
+         left join (select distinct r.resourceinstanceid,
+                                    r.officially_recognized_site,
+                                    r.registration_status,
+                                    jsonb_agg(
+                                            case when pe.authority is null then null else
+                                                jsonb_build_object(
+                                                        'authority', pe.authority,
+                                                        'auth_order', case when pe.authority = 'Provincial' then 0 when pe.authority = 'Municipal' then 1 else 2 end,
+                                                        'start_date', pe.designation_or_protection_start_date,
+                                                        'government_name', pe.government_name,
+                                                        'recognition_type', pe.recognition_type
+                                                    ) end order by case when pe.authority = 'Provincial' then 0 when pe.authority = 'Municipal' then 1 else 2 end,
+                                                designation_or_protection_start_date desc )->0 authorities
+--                        pe.designation_or_protection_end_date, registry_types, pe.reference_number, pe.protection_notes, pe.legal_instrument, pe.act_section, pe.recognition_type, pe.government_type
+                    from mv_bc_right r
+                             left join mv_protection_event pe on r.bc_right_id = pe.bc_right_id
+                    group by r.resourceinstanceid, r.officially_recognized_site, r.registration_status) br
+                   on bn.resourceinstanceid = br.resourceinstanceid
+         join mv_site_record_admin msra on bn.resourceinstanceid = msra.resourceinstanceid
+         left join (select resourceinstanceid, jsonb_agg(
+        jsonb_build_object(
+                'start_year', start_year,
+                'end_year', end_year,
+                'dates_approximate', dates_approximate,
+                'event', event,
+                'source', source,
+                'event_notes', event_notes)
+    ) chronology from mv_chronology where event = 'Construction' group by resourceinstanceid) mc on bn.resourceinstanceid = mc.resourceinstanceid
+         left join (select resourceinstanceid,
+                           jsonb_object_agg(state_period, array_to_string(functional_states, '; ') ) functional_states
+                    from mv_heritage_function
+                    group by resourceinstanceid) hf on bn.resourceinstanceid = hf.resourceinstanceid
+         left join (select resourceinstanceid,
+                           jsonb_object_agg(actor_type, array_to_string(actors, '; ')) actors
+                    from mv_construction_actors
+                    group by resourceinstanceid) ca on bn.resourceinstanceid = ca.resourceinstanceid
+         left join mv_site_boundary msb on bn.resourceinstanceid = msb.resourceinstanceid
+;
