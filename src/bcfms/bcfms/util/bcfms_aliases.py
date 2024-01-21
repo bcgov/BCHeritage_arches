47060d08f41e0b917b7805fc7e4d24028ee9e682
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2024-01-21 14:33:39 -0800

    650, 732 - Rework collection event and publication resource models

--- src/bcfms/bcfms/util/bcfms_aliases.py
+++ src/bcfms/bcfms/util/bcfms_aliases.py
@@ -1,11 +1,34 @@
 # Class to standardize the resource model node aliases
+class CollectionEventAliases:
+    START_YEAR = 'collection_start_year'
+    LOCATION_DESCRIPTOR = 'location_descriptor'
+    NTS_MAPSHEET_NAME = 'nts_mapsheet_name'
+
+    DETAILED_LOCATION = 'detailed_location'
+    SAMPLES_COLLECTED = 'samples_collected'
+
+
+class FossilSampleAliases:
+    FORMATION = 'geological_formation'
+    FORMATION_UNCERTAIN = 'geological_formation_uncertain'
+
+    SCIENTIFIC_NAME = 'scientific_name'
+    NAME_CONNECTOR = 'open_nomanclature_term'
+    OTHER_SCIENTIFIC_NAME = 'other_scientific_name'
+    FOSSIL_COMMON_NAME = 'fossil_common_name'
+    COMMON_NAME_UNCERTAIN = 'common_name_uncertain'
+
+    MINIMUM_TIME = 'minimum_time'
+    MINIMUM_TIME_UNCERTAIN = 'minimum_time_uncertain'
+
+
 class PublicationAliases:
     TITLE = 'title'
     AUTHORS = 'authors'
     NAME_TYPE = 'name_type'
     PUBLICATION_YEAR = 'year_of_publication'
     PUBLICATION_TYPE = 'publication_type'
-    JOURNAL_OR_PUBLICATION_NAME = 'journal_or_publication_name'
+    JOURNAL_OR_PUBLICATION_NAME = 'journal_or_volume_name'
 
 
 class FossilType:
