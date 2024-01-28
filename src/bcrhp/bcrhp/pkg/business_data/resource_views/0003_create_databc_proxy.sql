8ccf6528c32e838d6a978d16e2ea20ac94688b10
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2024-01-28 09:06:13 -0800

    807 - Handle first run when Data BC role doesn't exist. Make Data BC role name instance-specific to allow for shared DLVR/TEST infrastructure

--- src/bcrhp/bcrhp/pkg/business_data/resource_views/0003_create_databc_proxy.sql
+++ src/bcrhp/bcrhp/pkg/business_data/resource_views/0003_create_databc_proxy.sql
@@ -1,11 +1,22 @@
 -- Schema databc and DataBC proxy object must be created prior to running this script
-drop owned by proxy_databc cascade;
-drop role proxy_databc;
-create role proxy_databc password '{{ databc_api_password }}';
-alter role proxy_databc with login;
-alter role proxy_databc set search_path = databc;
-revoke all on schema public from proxy_databc;
-grant connect on database bcrhp to proxy_databc;
-grant usage on schema databc to proxy_databc;
-grant execute on function databc.get_first_address(p_resourceinstanceid uuid) to proxy_databc;
-grant select on databc.V_HISTORIC_ENVIRO_ONEROW_SITE to proxy_databc;
+DO
+$$
+    DECLARE
+        databc_role record;
+    BEGIN
+        for databc_role in (select rolname from pg_roles where rolname = '{{ db_databc_user }}') loop
+            Raise NOTICE 'Dropping role {{  db_databc_user }} and all associated grants';
+            drop owned by {{ db_databc_user }} cascade;
+            drop role {{ db_databc_user }};
+        end loop;
+    END
+$$ language plpgsql;
+
+create role {{ db_databc_user }} password '{{ databc_api_password }}';
+alter role {{ db_databc_user }} with login;
+alter role {{ db_databc_user }} set search_path = databc;
+revoke all on schema public from {{ db_databc_user }};
+grant connect on database bcrhp to {{ db_databc_user }};
+grant usage on schema databc to {{ db_databc_user }};
+grant execute on function databc.get_first_address(p_resourceinstanceid uuid) to {{ db_databc_user }};
+grant select on databc.V_HISTORIC_ENVIRO_ONEROW_SITE to {{ db_databc_user }};
