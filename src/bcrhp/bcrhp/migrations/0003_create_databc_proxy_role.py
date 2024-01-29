adae0ebcf9bdd9b1f8c5de24290fe528e76b8129
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2024-01-29 08:55:29 -0800

    807 - Add missing proxy schema app user privileges, fix scripts

--- src/bcrhp/bcrhp/migrations/0003_create_databc_proxy_role.py
+++ src/bcrhp/bcrhp/migrations/0003_create_databc_proxy_role.py
@@ -16,31 +16,29 @@ class Migration(migrations.Migration):
         drop schema databc;
     """
 
+    grant_databc_proxy_schema = """
+        grant all privileges on schema databc to {{ arches_db_user }};
+    """
+
+
     create_databc_proxy_role = """
         DO
         $$
             DECLARE
                 databc_role_exists boolean;
             BEGIN
-                select count(*) > 0 into databc_role_exists from pg_roles where rolname = '{{ db_databc_user }}'
+                select count(*) > 0 into databc_role_exists from pg_roles where rolname = '{{ db_databc_user }}';
                 if not databc_role_exists then
                     Raise NOTICE 'Creating role {{ db_databc_user }}';
                     create role {{ db_databc_user }} password '{{ db_databc_password }}';
                     alter role {{ db_databc_user }} with login;
                     alter role {{ db_databc_user }} set search_path = databc;
                     revoke all on schema public from {{ db_databc_user }};
-                    grant connect on database bcrhp to {{ db_databc_user }};
+                    grant connect on database {{ arches_db_name }} to {{ db_databc_user }};
                     grant usage on schema databc to {{ db_databc_user }};
                 else
                     Raise NOTICE 'Not creating role {{ db_databc_user }} - it already exists';
                 end if;
-                
-                
-                for databc_role in (select rolname from pg_roles where rolname = 'proxy_databc_dlvr') loop
-                    Raise NOTICE 'Dropping role proxy_databc_dlvr and all associated grants';
-                    drop owned by proxy_databc_dlvr cascade;
-                    drop role proxy_databc_dlvr;
-                end loop;
             END
         $$ language plpgsql;
     """
@@ -64,6 +62,10 @@ class Migration(migrations.Migration):
         RunPrivilegedSQL(
             create_databc_proxy_schema,
             drop_databc_proxy_schema,
+        ),
+        RunPrivilegedSQL(
+            grant_databc_proxy_schema,
+            migrations.RunSQL.noop,
         ),
         RunPrivilegedSQL(
             create_databc_proxy_role,
