c557a87b67eeb1cb3793749c6ee8cc2f5ca78605
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2024-02-08 19:12:37 -0800

    632 - Add borden number as map feature label

--- src/bcrhp/bcrhp/pkg/preliminary_sql/create_resource_overlay_package.sql
+++ src/bcrhp/bcrhp/pkg/preliminary_sql/create_resource_overlay_package.sql
@@ -5,6 +5,9 @@ declare
     l_heritage_site_geom_node_id text = '1b6235b0-0d0f-11ed-98c2-5254008afee6';
     l_heritage_site_legislative_act_id text = '1f28339e-3b93-11ee-b4c5-080027b7463b';
 
+    l_borden_number_id text = 'e5ecf044-0d06-11ed-86c8-5254008afee6';
+    l_borden_numer_nodegroup_id text = 'e5ecf044-0d06-11ed-86c8-5254008afee6';
+
     l_leg_act_authority_node_id text = '7789d580-3b87-11ee-a701-080027b7463b';
     data jsonb;
 begin
@@ -18,18 +21,23 @@ begin
                                and t.resourceinstanceid = p_resourceinstanceid /*(select nodegroupid
                                                     from nodes n
                                                     where n.nodeid = l_heritage_site_legislative_act_id::uuid)*/),
+             borden_number as (select resourceinstanceid,
+                                      tiledata -> l_borden_number_id ->'en'->>'value' borden_number/*, */
+                               from tiles
+                               where nodegroupid = l_borden_numer_nodegroup_id::uuid),
              authorities as (select resourceinstanceid,
                                     __arches_get_concept_label((tiledata ->> l_leg_act_authority_node_id)::uuid) authority/*, */
                              from tiles
                              where nodegroupid = '7789d580-3b87-11ee-a701-080027b7463b'::uuid /*((select nodegroupid
                                                    from nodes n
                                                    where n.nodeid = l_leg_act_authority_node_id::uuid))*/)
-        select jsonb_build_object('authorities', array_agg(distinct authority))
+        select jsonb_build_object('authorities', array_agg(distinct authority), 'borden_number', bn.borden_number)
         into data
         from heritage_site hs
+                 left join borden_number bn on bn.resourceinstanceid = hs.resourceinstanceid
                  left join authorities a on a.resourceinstanceid = hs.legislative_act_id
         where hs.resourceinstanceid = p_resourceinstanceid
-        group by hs.resourceinstanceid;
+        group by hs.resourceinstanceid, bn.borden_number;
     end if;
     return data;
 end;
