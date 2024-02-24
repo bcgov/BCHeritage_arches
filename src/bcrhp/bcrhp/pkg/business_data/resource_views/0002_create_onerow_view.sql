49cc60976e7bcb570015b27f2cfe2ba1081698f2
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2024-02-23 17:28:26 -0800

    814 - Create procedure to refresh all materialized views

--- src/bcrhp/bcrhp/pkg/business_data/resource_views/0002_create_onerow_view.sql
+++ src/bcrhp/bcrhp/pkg/business_data/resource_views/0002_create_onerow_view.sql
@@ -165,6 +165,27 @@ select b.resourceinstanceid,
        from heritage_site.site_boundary b join heritage_site.borden_number bn on b.resourceinstanceid = bn.resourceinstanceid;
 create index mv_sb_idx on mv_site_boundary(resourceinstanceid);
 
+create or replace procedure refresh_materialized_views() as
+$$
+BEGIN
+    refresh materialized view mv_bc_right;
+    refresh materialized view mv_bc_statement_of_significance;
+    refresh materialized view mv_borden_number;
+    refresh materialized view mv_chronology;
+    refresh materialized view mv_construction_actors;
+    refresh materialized view mv_government;
+    refresh materialized view mv_heritage_function;
+    refresh materialized view mv_legal_description;
+    refresh materialized view mv_property_address;
+    refresh materialized view mv_protection_event;
+    refresh materialized view mv_geojson_geoms;
+    refresh materialized view mv_site_boundary;
+    refresh materialized view mv_site_names;
+    refresh materialized view mv_site_protection_event;
+    refresh materialized view mv_site_record_admin;
+END
+$$ language plpgsql;
+
 create schema if not exists databc;
 
 drop function if exists databc.get_first_address cascade;
