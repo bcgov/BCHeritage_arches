7b3ae39810685516f14a9ee41832d3a63e1fa29e
brett <brett@qedsystems.ca>
2024-04-30 12:21:33 -0700

    First working copy - added missing bcrhp specific pip modules, copy common assets to project directory, tweak config.

--- src/bcrhp/docker/webpack/entrypoint.sh
+++ src/bcrhp/docker/webpack/entrypoint.sh
@@ -7,7 +7,7 @@ run_webpack() {
 	echo ""
 	cd ${APP_FOLDER}
     echo "Running Webpack"
-	exec sh -c "cd /web_root/project_name/project_name && yarn install && wait-for-it project_name:80 -t 1200 && yarn start"
+	exec sh -c "cd /web_root/bcrhp/bcrhp && yarn install && wait-for-it bcrhp:80 -t 1200 && yarn start"
 }
 
 run_webpack
