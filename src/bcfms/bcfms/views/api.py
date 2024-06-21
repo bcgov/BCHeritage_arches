7669158f7834b521d80174ffdc6bf215c2d4e973
brett <brett@qedsystems.ca>
2024-06-21 09:17:06 -0700

    971 - Add filter by Sandbox model 964 - Allow 4char report abbreviation, add missing import

--- src/bcfms/bcfms/views/api.py
+++ src/bcfms/bcfms/views/api.py
@@ -2,15 +2,14 @@ from bcfms.views.mvt_base import MVTConfig, MVT as MVTCommon
 from arches.app.views.api import APIBase
 from arches.app.utils.response import JSONResponse, JSONErrorResponse
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
-from bcfms.util.business_data_proxy import FossilSampleDataProxy, CollectionEventDataProxy
-
+from bcfms.util.business_data_proxy import FossilSampleDataProxy, CollectionEventDataProxy, IPADataProxy
 
 query_config = {
-    '5bfa1354-b6e1-11ee-9438-080027b7463b': ['size_categories'],
-    'c66518e2-10c6-11ec-adef-5254008afee6': ['name','ranking'],
-    'dd19c7c6-0202-11ed-a511-0050568377a0': ['name','ranking'],
-    '2336968c-1035-11ec-a3aa-5254008afee6': ['name','ranking'],
-    '4fe7beb2-1f3a-11ed-a99d-5254008afee6': ['name','ranking'],
+    '5bfa1354-b6e1-11ee-9438-080027b7463b': ['size_categories'],  # collection_event, collection_location
+    'c66518e2-10c6-11ec-adef-5254008afee6': ['name', 'ranking'],
+    'dd19c7c6-0202-11ed-a511-0050568377a0': ['name', 'ranking'],  # protected_site, area_boundary
+    '2336968c-1035-11ec-a3aa-5254008afee6': ['name', 'ranking'],  # fossil_site, area_boundary
+    '4fe7beb2-1f3a-11ed-a99d-5254008afee6': ['name', 'ranking'],
 }
 
 mvt_config = MVTConfig(query_config)
