0ccbdc14e07e0a426f123804a4080dd96b0e9265
brett <brett@qedsystems.ca>
2024-07-14 12:18:47 -0700

    947 - Create testing table of databc view for UAT

--- src/bcrhp/bcrhp/migrations/947_historic_site_one_line.py
+++ src/bcrhp/bcrhp/migrations/947_historic_site_one_line.py
@@ -16,7 +16,7 @@ from bcrhp.migrations.operations.privileged_sql import RunPrivilegedSQL
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bcrhp', '0003_create_databc_proxy_role'),
+        ('bcrhp', '947_create_testing_table'),
     ]
 
     operations = [
