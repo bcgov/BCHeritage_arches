96a843cdc35d7d864c165dfe3e3331f22743849f
brett <brett@qedsystems.ca>
2024-07-16 14:14:52 -0700

    981 - Add additional search terms, use data proxy to get values

--- src/bcfms/bcfms/util/custom_search_value.py
+++ src/bcfms/bcfms/util/custom_search_value.py
@@ -1,19 +1,25 @@
-from arches.app.datatypes import datatypes, concept_types
-from arches.app.search.elasticsearch_dsl_builder import Bool, Ids, Match, Nested, SimpleQueryString, QueryString, Terms, Term
-from bcfms.util.bcfms_aliases import CollectionEventAliases, FossilSampleAliases as fsa, FossilType, GraphSlugs
-from bcfms.util.scientific_terms_util import ScientificTermsFormatter
+from arches.app.search.elasticsearch_dsl_builder import (
+    Bool,
+    Ids,
+    Match,
+    Nested,
+    SimpleQueryString,
+    QueryString,
+    Terms,
+    Term,
+)
+from bcfms.util.bcfms_aliases import FossilSampleAliases as fsa, GraphSlugs
+from bcfms.util.business_data_proxy import (
+    FossilSampleDataProxy,
+    CollectionEventDataProxy,
+)
 
 
 class CustomSearchValue:
     custom_search_path = "custom_values"
     initialized = False
-    sample_graph = None
-    sample_node = None
-    resource_dt = None
-    concept_dt = None
-    collection_event_nodes = {}
-    sample_nodes = {}
-    fossil_type_nodes = {}
+    fossil_sample_proxy = None
+    collection_event_proxy = None
 
     def __init__(self):
         pass
@@ -21,62 +27,73 @@ class CustomSearchValue:
     @staticmethod
     def initialize():
         if not CustomSearchValue.initialized:
-            from arches.app.models.models import Node, GraphModel
-            CustomSearchValue.sample_graph = GraphModel.objects.filter(slug='fossil_sample', isresource=True).first()
-            CustomSearchValue.resource_dt = datatypes.ResourceInstanceDataType()
-            CustomSearchValue.concept_dt = concept_types.ConceptDataType()
-
-            CustomSearchValue.collection_event_nodes[CollectionEventAliases.SAMPLES_COLLECTED] = Node.objects.filter(alias=CollectionEventAliases.SAMPLES_COLLECTED).first()
-
-            CustomSearchValue.sample_nodes[fsa.SCIENTIFIC_NAME] = Node.objects.filter(alias=fsa.SCIENTIFIC_NAME).first()
-            CustomSearchValue.sample_nodes[fsa.NAME_CONNECTOR] = Node.objects.filter(alias=fsa.NAME_CONNECTOR).first()
-            CustomSearchValue.sample_nodes[fsa.OTHER_SCIENTIFIC_NAME] = Node.objects.filter(alias=fsa.OTHER_SCIENTIFIC_NAME).first()
-            CustomSearchValue.sample_nodes[fsa.FOSSIL_COMMON_NAME] = Node.objects.filter(alias=fsa.FOSSIL_COMMON_NAME).first()
-            CustomSearchValue.sample_nodes[fsa.MINIMUM_TIME] = Node.objects.filter(alias=fsa.MINIMUM_TIME).first()
-            CustomSearchValue.sample_nodes[fsa.MAXIMUM_TIME] = Node.objects.filter(alias=fsa.MAXIMUM_TIME).first()
-
-            CustomSearchValue.fossil_type_nodes[FossilType.NAME_TYPE] = Node.objects.filter(alias=FossilType.NAME_TYPE).first()
-            CustomSearchValue.fossil_type_nodes[FossilType.PARENT_NAME] = Node.objects.filter(alias=FossilType.PARENT_NAME).first()
-
+            CustomSearchValue.fossil_sample_proxy = FossilSampleDataProxy()
+            CustomSearchValue.collection_event_proxy = CollectionEventDataProxy()
+            CustomSearchValue.initialized = True
 
     @staticmethod
     def add_search_terms(resourceinstance, document, terms):
         CustomSearchValue.initialize()
-        import arches.app.models.tile as tile
         custom_values = set(())
 
-        # print("Slug: %s" % resourceinstance.graph.slug)
         if resourceinstance.graph.slug == GraphSlugs.COLLECTION_EVENT:
-            resourceinstance.load_tiles()
-            # print("Node values: %s" % str(resourceinstance.get_node_values(sample_node.name)))
-            samples = resourceinstance.get_node_values(CustomSearchValue.collection_event_nodes[CollectionEventAliases.SAMPLES_COLLECTED].name)
-            # print("Samples: %s" % samples)
-            # print("Resource Instance Tiles: %s" % resourceinstance.tiles)
-            if samples and len(samples) > 0:
-                sample_nodes = CustomSearchValue.sample_nodes
-                document[CustomSearchValue.custom_search_path] = []
-                # print("Maximum_time_node: %s" % maximum_time_node)
-                for sample in samples:
-                    # print("Sample: %s" % sample)
-                    custom_values.update(CustomSearchValue.get_fossil_types(sample))
+            sample_ids = CustomSearchValue.collection_event_proxy.get_sample_ids(
+                resourceinstance
+            )
 
-                    related_tiles = tile.Tile.objects.filter(resourceinstance_id=sample["resourceId"],
-                                                             nodegroup__nodegroupid=sample_nodes[fsa.MINIMUM_TIME].nodegroup_id).all()
-                    # print("Number of times(%s,%s): %s" %( sample["resourceId"], minimum_time_node.nodegroup_id, len(related_tiles)))
-                    for related_tile in related_tiles:
-                        custom_values.add(CustomSearchValue.concept_dt.get_display_value(related_tile, sample_nodes[fsa.MINIMUM_TIME]))
-                        custom_values.add(CustomSearchValue.concept_dt.get_display_value(related_tile, sample_nodes[fsa.MAXIMUM_TIME]))
+            custom_values |= set(
+                CustomSearchValue.fossil_sample_proxy.get_scientific_names_from_samples(
+                    sample_ids
+                )
+            )
+            custom_values |= set(
+                CustomSearchValue.fossil_sample_proxy.get_common_names_from_samples(
+                    sample_ids
+                )
+            )
+
+            custom_value_aliases = [
+                (fsa.MINIMUM_TIME, fsa.MINIMUM_TIME_UNCERTAIN),
+                (fsa.MAXIMUM_TIME, fsa.MAXIMUM_TIME_UNCERTAIN),
+                (fsa.GEOLOGICAL_GROUP, fsa.GEOLOGICAL_GROUP_UNCERTAIN),
+                (fsa.GEOLOGICAL_FORMATION, fsa.GEOLOGICAL_FORMATION_UNCERTAIN),
+                (fsa.GEOLOGICAL_MEMBER, fsa.GEOLOGICAL_MEMBER_UNCERTAIN),
+                (fsa.FOSSIL_ABUNDANCE, None),
+                (fsa.FOSSIL_SIZE_CATEGORY, None),
+                (fsa.FOSSIL_SAMPLE_SIGNIFICANT, None),
+                (fsa.STORAGE_REFERENCE, None),
+            ]
+            for alias in custom_value_aliases:
+                custom_values |= set(
+                    CustomSearchValue.fossil_sample_proxy.get_values_from_samples(
+                        sample_ids, alias[0], alias[1]
+                    )
+                )
 
         elif resourceinstance.graph.slug == GraphSlugs.FOSSIL_SAMPLE:
-            custom_values.update(CustomSearchValue.get_fossil_types(resourceinstance))
+            custom_values |= set(
+                CustomSearchValue.fossil_sample_proxy.get_scientific_names_from_samples(
+                    [resourceinstance]
+                )
+            )
+            custom_values |= set(
+                CustomSearchValue.fossil_sample_proxy.get_common_names_from_samples(
+                    [resourceinstance]
+                )
+            )
+            # custom_values.update(CustomSearchValue.get_fossil_types(resourceinstance))
 
         elif resourceinstance.graph.slug == GraphSlugs.FOSSIL_TYPE:
             # Need to add parent if it is a species
-            custom_values.add(resourceinstance.get_descriptor(descriptor="name", context={}))
+            custom_values.add(
+                resourceinstance.get_descriptor(descriptor="name", context={})
+            )
 
         elif resourceinstance.graph.slug == GraphSlugs.PUBLICATION:
             # Need to add parent if it is a volume
-            custom_values.add(resourceinstance.get_descriptor(descriptor="name", context={}))
+            custom_values.add(
+                resourceinstance.get_descriptor(descriptor="name", context={})
+            )
 
         # print("Adding custom values: %s" % custom_values)
         if CustomSearchValue.custom_search_path not in document:
@@ -84,45 +101,9 @@ class CustomSearchValue:
 
         for custom_value in custom_values:
             if custom_value:
-                # print("Related node value: %s" % custom_value)
-                document[CustomSearchValue.custom_search_path].append({"custom_value": custom_value})
-
-    @staticmethod
-    def get_parent_fossil_type(fossil_type):
-        import arches.app.models.tile as tile
-
-        custom_values = set(())
-        print("name: %s" % fossil_type.get_descriptor("name", context={}))
-        type_nodes = CustomSearchValue.fossil_type_nodes
-        related_tiles = tile.Tile.objects.filter(resourceinstance_id=fossil_type.resourceinstanceid,
-                                                 nodegroup__nodegroupid=type_nodes[FossilType.PARENT_NAME].nodegroup_id).all()
-        for related_tile in related_tiles:
-            custom_values.add(CustomSearchValue.resource_dt.get_display_value(related_tile, type_nodes[FossilType.PARENT_NAME]))
-
-        return custom_values
-
-    @staticmethod
-    def get_fossil_types(sample):
-        import arches.app.models.tile as tile
-        sample_nodes = CustomSearchValue.sample_nodes
-        custom_values = set(())
-        # print("Sample: %s" % type(sample))
-        sample_id = sample["resourceId"] if type(sample) == dict else sample.resourceinstanceid
-        related_tiles = tile.Tile.objects.filter(resourceinstance_id=sample_id,
-                                                 nodegroup__nodegroupid=sample_nodes[fsa.SCIENTIFIC_NAME].nodegroup_id).all()
-        # print("Related tile: %s" % sample)
-        for related_tile in related_tiles:
-            sci_name_val = CustomSearchValue.resource_dt.get_display_value(related_tile, sample_nodes[fsa.SCIENTIFIC_NAME])
-            conn_val = CustomSearchValue.concept_dt.get_display_value(related_tile, sample_nodes[fsa.NAME_CONNECTOR])
-            other_name_val = CustomSearchValue.resource_dt.get_display_value(related_tile, sample_nodes[fsa.OTHER_SCIENTIFIC_NAME])
-            name_val = ScientificTermsFormatter.format_scientific_name(sci_name_val, conn_val, other_name_val)
-            common_val = CustomSearchValue.resource_dt.get_display_value(related_tile, sample_nodes[fsa.FOSSIL_COMMON_NAME])
-            custom_values.add(name_val)
-            custom_values.add(common_val)
-
-        #print("Returning: %s" % custom_values)
-        return custom_values
-
+                document[CustomSearchValue.custom_search_path].append(
+                    {"custom_value": custom_value}
+                )
 
     @staticmethod
     def add_search_filter(search_query, term):
@@ -137,8 +118,20 @@ class CustomSearchValue:
 
         document_key = CustomSearchValue.custom_search_path
         custom_filter = Bool()
-        custom_filter.should(Match(field="%s.custom_value" % document_key, query=term["value"], type="phrase_prefix"))
-        custom_filter.should(Match(field="%s.custom_value.folded" % document_key, query=term["value"], type="phrase_prefix"))
+        custom_filter.should(
+            Match(
+                field="%s.custom_value" % document_key,
+                query=term["value"],
+                type="phrase_prefix",
+            )
+        )
+        custom_filter.should(
+            Match(
+                field="%s.custom_value.folded" % document_key,
+                query=term["value"],
+                type="phrase_prefix",
+            )
+        )
         nested_custom_filter = Nested(path=document_key, query=custom_filter)
         # return nested_custom_filter
         if term["inverted"]:
@@ -149,13 +142,15 @@ class CustomSearchValue:
 
     @staticmethod
     def get_custom_search_config():
-        return {"type": "nested",
-                "properties": {
-                    "custom_value": {"type": "text",
-                                     "fields": {
-                                         "raw": {"type": "keyword", "ignore_above": 256},
-                                         "folded": {"type": "text", "analyzer": "folding"}
-                                     },
-                                     }
+        return {
+            "type": "nested",
+            "properties": {
+                "custom_value": {
+                    "type": "text",
+                    "fields": {
+                        "raw": {"type": "keyword", "ignore_above": 256},
+                        "folded": {"type": "text", "analyzer": "folding"},
+                    },
                 }
+            },
         }
