8a69913a7166116e923976e075933feecb4d9310
brett <brett@qedsystems.ca>
2024-07-22 14:35:22 -0700

    Initial refactor to add DataBC views

---
+++ src/bcfms/bcfms/migrations/0002_create_view_support_functions.py
@@ -0,0 +1,183 @@
+from django.db import migrations
+from bcfms.migrations.operations.privileged_sql import RunPrivilegedSQL
+
+
+class Migration(migrations.Migration):
+    dependencies = [('bcfms',
+                     '0001_create_databc_proxy_role')]
+
+    create_create_node_aliases = """
+        create or replace procedure __bc_create_node_aliases(p_graph_slug text, p_schema_name text default null) as
+        $$
+        DECLARE
+            node_rec record;
+        BEGIN
+            for node_rec in select n.* from nodes n join graphs g on n.graphid = g.graphid where g.slug = p_graph_slug loop
+                      raise Notice '%', node_rec.alias;
+                      execute format('create or replace function %I.%I() returns uuid as $body$ begin return ''%s''::uuid; end $body$ language plpgsql', coalesce(p_schema_name, p_graph_slug), node_rec.alias||'_nodeid', node_rec.nodeid);
+                      execute format('create or replace function %I.%I() returns uuid as $body$ begin return ''%s''::uuid; end $body$ language plpgsql', coalesce(p_schema_name, p_graph_slug), node_rec.alias||'_nodegroupid', node_rec.nodegroupid);
+                end loop;
+        END $$ language plpgsql;
+        """
+    drop_create_node_aliases = """
+        drop procedure if exists __bc_create_node_aliases;
+        """
+
+    create_text_from_tile = """
+        create or replace function __bc_text_from_tile(tiledata jsonb, nodeid uuid) returns text as
+        $$
+        DECLARE
+        BEGIN
+            return tiledata[nodeid::text]->'en'->>'value';
+        END $$ language plpgsql;
+        """
+    drop_text_from_tile = """
+        drop function if exists __bc_text_from_tile;
+        """
+
+    create_point_from_tile = """
+        create or replace function __bc_point_from_tile(tiledata jsonb, nodeid uuid) returns text as
+        $$
+        DECLARE
+        BEGIN
+            return tiledata[nodeid::text]->'features'->0->'geometry'->>'coordinates';
+        END $$ language plpgsql;
+        """
+    drop_point_from_tile = """
+        drop function if exists __bc_point_from_tile;
+        """
+
+    create_ref_from_tile = """
+        create or replace function __bc_ref_from_tile(tiledata jsonb, nodeid uuid) returns setof uuid as
+        $$
+        DECLARE
+        BEGIN
+            if (jsonb_typeof(tiledata->(nodeid::text)) = 'null' or tiledata->>(nodeid::text)::text = '') then
+                return query select null::uuid;
+            else
+                return query select (jsonb_array_elements(tiledata[nodeid::text])->>'resourceId')::uuid;
+            end if;
+        EXCEPTION WHEN OTHERS THEN
+            raise EXCEPTION 'Unable to parse %', tiledata;
+        END $$ language plpgsql;
+        """
+
+    drop_ref_from_tile = """
+        drop function if exists __bc_ref_from_tile;
+        """
+
+    create_format_uncertainty = """
+        create or replace function __bc_format_uncertainty(tiledata jsonb, nodeid uuid, uncertainty_nodeid uuid) returns text as
+        $$
+        DECLARE
+            nodeval text;
+            is_uncertain boolean;
+        BEGIN
+
+            nodeval = __arches_get_node_display_value(tiledata, nodeid);
+            if (nodeval is null or nodeval = '') then
+                return '';
+            else
+                is_uncertain = (tiledata->uncertainty_nodeid::text) is not null and (tiledata->uncertainty_nodeid::text)::boolean;
+                return nodeval || (case when is_uncertain then ' ?' else '' end);
+            end if;
+        END $$ language plpgsql;
+        """
+
+    drop_format_uncertainty = """
+        drop function if exists __bc_format_uncertainty;
+        """
+
+    create_unique_array = """
+        create or replace function __bc_unique_array(p_inarray anyarray) returns anyarray as
+        $$
+        DECLARE
+            uq_array text[];
+        BEGIN
+            with uq as (select distinct val from unnest(p_inarray) val
+                                            where coalesce(val,'') <> '' order by 1)
+            select array_agg(val) into uq_array from uq;
+            return uq_array;
+        END $$ language plpgsql;
+        """
+    drop_unique_array = """
+        drop function if exists __bc_unique_array;
+        """
+
+
+    create_format_scientific_name = """
+        create or replace function __bc_format_scientific_name(name text, name_rank text, connector text, other_name text, other_name_rank text) returns text as
+        $$
+        DECLARE
+        BEGIN
+            return trim(replace(coalesce(name, '') ||  ' ' ||
+                                coalesce(connector, '') || ' ' ||
+                                coalesce(other_name, '')
+                , '  ', ' ')
+
+                );
+        END $$ language plpgsql;
+        """
+
+    drop_format_scientific_name = """
+        drop function if exists __bc_format_scientific_name cascade;
+        """
+
+    drop_databc_proxy_schema = """
+        drop schema databc;
+    """
+
+    grant_databc_proxy_schema = """
+        grant all privileges on schema databc to {{ arches_db_user }};
+    """
+
+    create_databc_proxy_role = """
+        DO
+        $$
+            DECLARE
+                databc_role_exists boolean;
+            BEGIN
+                select count(*) > 0 into databc_role_exists from pg_roles where rolname = '{{ db_databc_user }}';
+                if not databc_role_exists then
+                    Raise NOTICE 'Creating role {{ db_databc_user }}';
+                    create role {{ db_databc_user }} password '{{ db_databc_password }}';
+                else
+                    Raise NOTICE 'Not creating role {{ db_databc_user }} - it already exists';
+                end if;
+                alter role {{ db_databc_user }} with login;
+                alter role {{ db_databc_user }} set search_path = databc,public;
+                revoke all on schema public from {{ db_databc_user }};
+                grant connect on database {{ arches_db_name }} to {{ db_databc_user }};
+                grant usage on schema databc to {{ db_databc_user }};
+                grant select on geometry_columns TO {{ db_databc_user }};
+                grant select on geography_columns TO {{ db_databc_user }};
+                grant select on spatial_ref_sys TO {{ db_databc_user }};
+            END
+        $$ language plpgsql;
+    """
+
+    drop_databc_proxy_role = """
+        DO
+        $$
+            DECLARE
+                databc_role record;
+            BEGIN
+                for databc_role in (select rolname from pg_roles where rolname = '{{ db_databc_user }}') loop
+                    Raise NOTICE 'Dropping role {{ db_databc_user }} and all associated grants';
+                    drop owned by {{ db_databc_user }} cascade;
+                    drop role {{ db_databc_user }};
+                end loop;
+            END
+        $$ language plpgsql;
+    """
+
+    operations = [
+        RunPrivilegedSQL(create_create_node_aliases, drop_create_node_aliases),
+        RunPrivilegedSQL(create_text_from_tile, drop_text_from_tile),
+        RunPrivilegedSQL(create_point_from_tile, drop_point_from_tile),
+        RunPrivilegedSQL(create_ref_from_tile, drop_ref_from_tile),
+        RunPrivilegedSQL(create_format_uncertainty, drop_format_uncertainty),
+        RunPrivilegedSQL(create_unique_array, drop_unique_array),
+        RunPrivilegedSQL(create_format_scientific_name, drop_format_scientific_name),
+        RunPrivilegedSQL(create_databc_proxy_role, drop_databc_proxy_role),
+    ]
