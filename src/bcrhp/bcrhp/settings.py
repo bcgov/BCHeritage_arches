5a432a9240bef8877b8e9d546562ac5ebb799a7a
brett <brett@qedsystems.ca>
2024-07-22 20:41:49 -0700

    1029 - Fix media root

--- src/bcrhp/bcrhp/settings.py
+++ src/bcrhp/bcrhp/settings.py
@@ -238,7 +238,7 @@ WSGI_APPLICATION = 'bcrhp.wsgi.application'
 
 # URL that handles the media served from MEDIA_ROOT, used for managing stored files.
 # It must end in a slash if set to a non-empty value.
-MEDIA_URL = '/files/'
+MEDIA_URL = '/bcrhp/files/'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = os.path.join(APP_ROOT)
