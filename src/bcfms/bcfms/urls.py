ab1f19c6533be6a61d588a70c9729f5710916f28
brett <brett@qedsystems.ca>
2024-10-15 18:13:40 -0700

    1084 - Interim commit - docker configuration update

--- src/bcfms/bcfms/urls.py
+++ src/bcfms/bcfms/urls.py
@@ -96,7 +96,18 @@ urlpatterns = [
         name="unauthorized",
     ),
     bc_url_resolver,
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-if settings.SHOW_LANGUAGE_SWITCH is True:
-    urlpatterns = i18n_patterns(*urlpatterns)
+# Ensure Arches core urls are superseded by project-level urls
+urlpatterns.append(path('', include('arches.urls')))
+
+# Adds URL pattern to serve media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Only handle i18n routing in active project. This will still handle the routes provided by Arches core and Arches applications,
+# but handling i18n routes in multiple places causes application errors.
+if settings.ROOT_URLCONF == __name__:
+    if settings.SHOW_LANGUAGE_SWITCH is True:
+        urlpatterns = i18n_patterns(*urlpatterns)
+
+    urlpatterns.append(path("i18n/", include("django.conf.urls.i18n")))
