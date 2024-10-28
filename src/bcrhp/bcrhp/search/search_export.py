6e0f24401d4498379eed23f40c6a1fdb6013fcdd
brett <brett@qedsystems.ca>
2024-10-28 09:57:26 -0700

    1036 - Show all fields in CSV for internal users, regardless of node export settings

--- src/bcrhp/bcrhp/search/search_export.py
+++ src/bcrhp/bcrhp/search/search_export.py
@@ -1,21 +1,34 @@
 from arches.app.search.search_export import SearchResultsExporter
 from django.db import connection
+from arches.app.models import models
+from bcrhp.util.bcrhp_aliases import GraphSlugs
+
 
 class BCRHPSearchResultsExporter(SearchResultsExporter):
 
     def to_csv(self, instances, headers, name):
-        if name == "Heritage Site" and self.search_request.user.username == "anonymous":
-            collection_ids = [o['resourceid'] for o in instances]
+        if name == "Heritage Site":
+            if self.search_request.user.username == "anonymous":  # Anonymous users use the view
+                collection_ids = [o["resourceid"] for o in instances]
 
-            with connection.cursor() as cur:
-                cur.execute("""
-                    select *
-                    from heritage_site.csv_export
-                    where array_position(%s, site_id::text) is not null
-                """, [collection_ids])
-                columns = [desc[0] for desc in cur.description]
-                results = [dict(zip(columns, row)) for row in cur.fetchall()]
-                return super().to_csv(results, columns, name)
+                with connection.cursor() as cur:
+                    cur.execute(
+                        """
+                        select *
+                        from heritage_site.csv_export
+                        where array_position(%s, site_id::text) is not null
+                    """,
+                        [collection_ids],
+                    )
+                    columns = [desc[0] for desc in cur.description]
+                    results = [dict(zip(columns, row)) for row in cur.fetchall()]
+                    return super().to_csv(results, columns, name)
+            else:  # We're an authenticated user - show all the nodes
+                graph = models.GraphModel.objects.get(slug=GraphSlugs.HERITAGE_SITE)
+                headers = self.return_ordered_header(graph.graphid, "csv")
+                headers.append("resourceid")
+                return super().to_csv(instances, headers, name)
+
+        headers.append("resourceid")
 
         return super().to_csv(instances, headers, name)
-
