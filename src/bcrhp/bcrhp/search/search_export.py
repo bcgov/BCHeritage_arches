de6afb3d9111d08025f563ef576cff8992740328
brett <brett@qedsystems.ca>
2024-10-29 12:29:37 -0700

    1036 - Don't filter by exportable=True

--- src/bcrhp/bcrhp/search/search_export.py
+++ src/bcrhp/bcrhp/search/search_export.py
@@ -25,8 +25,70 @@ class BCRHPSearchResultsExporter(SearchResultsExporter):
                     return super().to_csv(results, columns, name)
             else:  # We're an authenticated user - show all the nodes
                 graph = models.GraphModel.objects.get(slug=GraphSlugs.HERITAGE_SITE)
-                headers = self.return_ordered_header(graph.graphid, "csv")
+                headers = self.return_ordered_header_for_all(graph.graphid)
                 headers.append("resourceid")
                 return super().to_csv(instances, headers, name)
 
         return super().to_csv(instances, headers, name)
+
+    def return_ordered_header_for_all(self, graphid):
+        # Copy of the SearchResultsExporter.return_ordered_header() function without filtering by exportable=True
+
+        subcard_list_with_sort = []
+        all_cards = models.CardModel.objects.filter(graph=graphid).prefetch_related("nodegroup")
+        all_card_list_with_sort = list(all_cards.exclude(sortorder=None).order_by("sortorder"))
+        card_list_no_sort = list(all_cards.filter(sortorder=None))
+        sorted_card_list = []
+
+        # Work out which cards with sort order are sub cards by looking at the
+        # related nodegroup's parent nodegroup value
+
+        for card_with_sortorder in all_card_list_with_sort:
+            if card_with_sortorder.nodegroup.parentnodegroup_id is None:
+                sorted_card_list.append(card_with_sortorder)
+            else:
+                subcard_list_with_sort.append(card_with_sortorder)
+
+        # Reverse set to allow cards with sort and a parent nodegroup
+        # to be injected into the main list in the correct order i.e. above
+        # cards with no sort order and according to the sort order as cards are
+        # injected just below the top card.
+
+        subcard_list_with_sort.sort(key=lambda x: x.sortorder, reverse=True)
+
+        def order_cards(subcards_added=True):
+            if subcards_added == True:
+                subcards_added = False
+                unsorted_subcards_added = self.insert_subcard_below_parent_card(sorted_card_list, card_list_no_sort, subcards_added)
+                sorted_subcards_added = self.insert_subcard_below_parent_card(sorted_card_list, subcard_list_with_sort, unsorted_subcards_added)
+                order_cards(sorted_subcards_added)
+
+        order_cards()
+
+        # Create a list of nodes within each card and order them according to sort
+        # order then add them to the main list of
+
+        ordered_list_all_nodes = []
+        for sorted_card in sorted_card_list:
+            card_node_objects = list(models.CardXNodeXWidget.objects.filter(card_id=sorted_card.cardid).prefetch_related("node"))
+            if len(card_node_objects) > 0:
+                nodes_in_card = []
+                for card_node_object in card_node_objects:
+                    if card_node_object.node.datatype != "semantic":
+                        nodes_in_card.append(card_node_object)
+                node_object_list_sorted = sorted(nodes_in_card, key=lambda x: x.sortorder)
+                for sorted_node_object in node_object_list_sorted:
+                    ordered_list_all_nodes.append(sorted_node_object)
+
+        # Build the list of headers (in correct format for return file format) to be returned
+        # from the ordered list of nodes, only where the exportable tag is true
+
+        headers = []
+        node_id_list = []
+        for ordered_node in ordered_list_all_nodes:
+            node_object = ordered_node.node
+            if node_object.nodeid not in node_id_list:
+                headers.append(node_object.name)
+                node_id_list.append(node_object.nodeid)
+
+        return headers
