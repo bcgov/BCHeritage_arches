49c6e43ee2bf4703093bfc0b386ad6727acc980c
brett <brett@qedsystems.ca>
2024-11-14 12:39:58 -0800

    1146 - Fix details tabs - 7.6 API data structure changes

--- src/bcfms/bcfms/media/js/reports/default.js
+++ src/bcfms/bcfms/media/js/reports/default.js
@@ -2,9 +2,6 @@ define([
         'knockout',
         'viewmodels/bcfms-report',
         'templates/views/report-templates/bcfms_default.htm',
-        'templates/views/report-templates/details/fossil_sample.htm',
-        'templates/views/report-templates/details/publication.htm',
-        'templates/views/report-templates/details/collection_event.htm',
 ], function (ko, ReportViewModel, defaultTemplate) {
     const viewModel = function(params) {
         params.configKeys = [];
