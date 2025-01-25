e462276144929d07a23f56ead3534c575a3ce4b6
brett <brett@qedsystems.ca>
2025-01-25 08:59:19 -0800

    1086 - Fix docker config for Arches Core 7.6 and Arches Common App

--- src/bcfms/docker/webpack/entrypoint.sh
+++ src/bcfms/docker/webpack/entrypoint.sh
@@ -7,7 +7,7 @@ run_webpack() {
 	echo ""
 	cd ${APP_FOLDER}
     echo "Running Webpack"
-	exec sh -c "cd /web_root/bcfms/bcfms && yarn install && wait-for-it bcfms:80 -t 1200 && yarn start"
+	exec sh -c "cd /web_root/bcfms && npm install && wait-for-it bcfms:80 -t 1200 && npm run start"
 }
 
 run_webpack
